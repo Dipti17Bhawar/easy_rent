be67075a100022e1081c20db2260954ecba01dff
Dipti17Bhawar <diptibhawar@gmail.com>
2026-06-12 12:00:23 +0530

    code for data.jsx

---
+++ src/data.jsx
@@ -0,0 +1,40 @@
+const flowersData = [
+  {
+    id: 1,
+    name: 'Rose Bouquet',
+    price: 499,
+    image: 'https://images.unsplash.com/photo-1519378058457-4c29a0a2efac'
+  },
+  {
+    id: 2,
+    name: 'Tulip Basket',
+    price: 699,
+    image: 'https://images.unsplash.com/photo-1468327768560-75b778cbb551'
+  },
+  {
+    id: 3,
+    name: 'Sunflower Pack',
+    price: 599,
+    image: 'https://images.unsplash.com/photo-1470509037663-253afd7f0f51'
+  },
+  {
+    id: 4,
+    name: 'Lily Collection',
+    price: 799,
+    image: 'https://images.unsplash.com/photo-1490750967868-88aa4486c946'
+  },
+  {
+    id: 5,
+    name: 'Orchid Premium',
+    price: 999,
+    image: 'https://images.unsplash.com/photo-1501004318641-b39e6451bec6'
+  },
+  {
+    id: 6,
+    name: 'Lavender Special',
+    price: 650,
+    image: 'https://images.unsplash.com/photo-1462275646964-a0e3386b89fa'
+  }
+]
+
+export default flowersData
